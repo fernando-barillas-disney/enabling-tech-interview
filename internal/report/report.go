4695170b761c8712c09ef764168af8c00aa3164d
Fernando Barillas <fernando.barillas@disney.com>
2023-12-12 14:29:11 -0800

    Update instructions

--- internal/report/report.go
+++ internal/report/report.go
@@ -1,11 +1,11 @@
 package report
 
 type CustomerOrder struct {
-	// TODO: Implement me
+	// TODO: Implement me DURING INTERVIEW ONLY
 }
 
 func GetCustomerOrders() (*[]CustomerOrder, error) {
-	// TODO: Implement me
+	// TODO: Implement me DURING INTERVIEW ONLY
 
 	return &[]CustomerOrder{}, nil
 }
